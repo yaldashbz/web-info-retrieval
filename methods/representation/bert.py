78b667f84559bb9e98e306454fe9c4feadd17480
yaldashbz <yaldashabanzadeh@gmail.com>
2022-06-12 00:23:11 +0430

    feat: add save

--- methods/representation/bert.py
+++ methods/representation/bert.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import pandas as pd
 from typing import List
@@ -8,12 +9,27 @@ from methods.representation.base import BaseRepresentation
 
 
 class BertRepresentation(BaseRepresentation):
-    def __init__(self, data):
+    _PATH = 'embeddings'
+    _FILE = 'bert_embeddings.json'
+
+    def __init__(self, data, load: bool = False):
         super().__init__(data)
         contents = self.prepare_data()
-        self.model = SentenceTransformer('distilbert-base-nli-stsb-mean-tokens')
-        self._to_cuda()
-        self.df = self._get_embeddings(contents)
+        if not load:
+            self.model = SentenceTransformer('distilbert-base-nli-stsb-mean-tokens')
+            self._to_cuda()
+            self.df = self._get_embeddings(contents)
+            self._save_embeddings()
+        else:
+            self.df = self._load_embeddings()
+
+    def _load_embeddings(self):
+        return pd.read_json(os.path.join(self._PATH, self._FILE))
+
+    def _save_embeddings(self):
+        if not os.path.exists(self._PATH):
+            os.mkdir(self._PATH)
+        self.df.to_json(os.path.join(self._PATH, self._FILE))
 
     def prepare_data(self) -> List:
         return get_contents(self.data)
@@ -26,5 +42,9 @@ class BertRepresentation(BaseRepresentation):
             self.model = self.model.to(torch.device('cuda'))
 
     def _get_embeddings(self, contents):
-        embeddings = self.model.encode(contents, show_progress_bar=True, normalize_embeddings=True)
+        embeddings = self.model.encode(
+            contents,
+            show_progress_bar=True,
+            normalize_embeddings=True
+        )
         return pd.DataFrame(embeddings)
--- methods/representation/tf_idf.py
+++ methods/representation/tf_idf.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 from typing import List
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -7,18 +8,33 @@ from methods.representation.base import BaseRepresentation
 
 
 class TFIDFRepresentation(BaseRepresentation):
-    def __init__(self, data):
+    _PATH = 'matrices'
+    _FILE = 'tfidf.json'
+
+    def __init__(self, data, load: bool = False):
         super().__init__(data)
         contents = self.prepare_data()
         self.tfidf = TfidfVectorizer(use_idf=True, norm='l2', analyzer='word')
-        self.matrix = self.tfidf.fit_transform(contents)
-        self.vocab = self.tfidf.get_feature_names_out()
+        if not load:
+            self.matrix = self.tfidf.fit_transform(contents)
+            self.vocab = self.tfidf.get_feature_names_out()
+            self.df = pd.DataFrame(data=self.matrix.toarray(), columns=self.vocab)
+            self._save()
+        else:
+            self.df = self._load()
+            self.matrix = self.df.to_numpy()
+            self.vocab = list(self.df.columns)
+
+    def _load(self):
+        return pd.read_json(os.path.join(self._PATH, self._FILE))
+
+    def _save(self):
+        if not os.path.exists(self._PATH):
+            os.mkdir(self._PATH)
+        self.df.to_json(os.path.join(self._PATH, self._FILE))
 
     def prepare_data(self) -> List:
         return get_contents(self.data)
 
     def represent(self) -> pd.DataFrame:
-        return pd.DataFrame(
-            data=self.matrix.toarray(),
-            columns=self.vocab
-        )
+        return self.df
