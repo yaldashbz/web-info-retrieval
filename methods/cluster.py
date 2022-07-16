c389e78e0c9f064560dd1e8e3040251bb7a81214
yaldashbz <yaldashabanzadeh@gmail.com>
2022-07-16 22:49:12 +0200

    feat: add cluster save

--- methods/cluster.py
+++ methods/cluster.py
@@ -1,3 +1,5 @@
+import os
+import pickle
 from typing import Tuple
 
 import pandas as pd
@@ -17,18 +19,57 @@ _representations = {
 
 
 class ContentKMeanCluster:
-    def __init__(self, data, method: str = 'tf-idf', **repr_kwargs):
+    _PATH = '../models/kmeans'
+    _MODEL = 'kmeans.pkl'
+    _RESULT = 'result.json'
+
+    def __init__(self, data, method: str = 'tf-idf', load: bool = True, **repr_kwargs):
+        if not os.path.exists(self._PATH):
+            os.mkdir(self._PATH)
+
         self.data = data
         self.representation = _representations[method](data=data, **repr_kwargs)
         self.represented_df = self.representation.represent()
-        self.k_means = None
+        self.k_means = None if not load else self.load_model(self.model_path)
+        self.result_df = None if not load else self.load_result(self.result_path)
+
+    @property
+    def model_path(self):
+        return os.path.join(self._PATH, self._MODEL)
+
+    @property
+    def result_path(self):
+        return os.path.join(self._PATH, self._RESULT)
 
     def run(self, k: int = 2):
         self.k_means = KMeans(
             n_clusters=k,
             random_state=1
         ).fit(self.represented_df)
+        self.save_model(self.model_path)
         return self.k_means
+
+    def save_model(self, path: str):
+        pickle.dump(self.k_means, open(path, 'wb'))
+
+    def save_result(self, path: str):
+        self.result_df.to_json(path)
+
+    @classmethod
+    def load_model(cls, path: str):
+        return pickle.load(open(path, 'rb'))
+
+    @classmethod
+    def load_result(cls, path: str):
+        return pd.read_json(path)
+
+    def cluster(self, query: str):
+        vector = self.representation.embed(query)
+        labels = self.k_means.predict([vector])
+        return labels[0]
+
+    def get_clustered_data(self, cluster_id):
+        return self.result_df[self.result_df.cluster_id == cluster_id]
 
     def _get_result(self):
         result = self.data
@@ -43,6 +84,7 @@ class ContentKMeanCluster:
         result_df = pd.DataFrame(result)
         result_df['content'] = result_df['tokens'].apply(get_content)
         result_df.pop('tokens')
+        self.result_df = result_df
         return result_df
 
     def elbow_visualize(self, k_range: Tuple[int, int]):
